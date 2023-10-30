9592d25b26cf0ac1427faf98b495bcd1d0c09ed9
Daniel Claesson <dacl@rocketmail.com>
2023-10-30 17:57:11 +0100

    Just minor updates to the previously done CodeAlongs, refreshing the notes.

--- CodeAlong/db_sandbox.py
+++ CodeAlong/db_sandbox.py
@@ -1,3 +1,27 @@
-# sandlåda för databehandlingskursen:
+# sandbox for databehandlingskursen:
 
 #%%
+# skapar en dataframe fr�n en dict
+# printar den
+# df.info()
+# skapar ett series object av Col_A
+
+import pandas as pd
+
+data = {
+        'Col_A':[1,2,3,4,5],
+        'Col_B':["a","b","c","d","e"]
+        }
+
+my_df = pd.DataFrame(data)
+
+print(my_df)
+my_df.info()
+
+my_series = pd.Series(my_df['Col_A'])
+print(my_series)
+type(my_series)
+
+my_df.describe()
+
+print(my_df['Col_B'])
