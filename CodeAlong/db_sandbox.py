46ac1aca5f59ea21a64938bf1af2205acf4a69c9
Daniel Claesson <dacl@rocketmail.com>
2023-11-02 13:40:52 +0100

    More sandbox stuff.

--- CodeAlong/db_sandbox.py
+++ CodeAlong/db_sandbox.py
@@ -25,3 +25,47 @@ type(my_series)
 my_df.describe()
 
 print(my_df['Col_B'])
+
+#%%
+my_list = ['name','email','job_title']
+
+print(my_list[0])
+
+for item in range(len(my_list)):
+    my_list[item] = my_list[item].title()
+    
+print(my_list)
+
+my_list[0] = 'NamE'
+
+print(my_list)
+
+#%%
+import pandas as pd
+
+employees = pd.read_json("C:/Users/danie/Documents/Github/Databehandling_Daniel_Claesson/Data/employees.json")
+
+employees
+
+my_cols = employees.columns
+
+my_cols_update = []
+
+for item in my_cols:
+    print(item)
+    my_cols_update.append(item.title())
+
+print(my_cols_update)
+
+employees.columns = my_cols_update
+
+employees
+#%%
+title = "job_title"
+
+if "_" in title:
+    title.replace("_", " ")
+
+print(title)
+
+"job_title".replace("_", " ")
